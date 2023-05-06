07565d5d4975e39339d4da4572f9b86c4ea42193
ricky-kiva <ricky.kiva@gmail.com>
2023-05-06 23:42:57 +0700

    add End-to-End Test

---
+++ app/src/androidTest/java/com/dicoding/newsapp/ui/list/HomeActivityTest.kt
@@ -0,0 +1,64 @@
+package com.dicoding.newsapp.ui.list
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.dicoding.newsapp.utils.EspressoIdlingResource
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import com.dicoding.newsapp.R
+import com.dicoding.newsapp.ui.detail.NewsDetailActivity
+
+class HomeActivityTest {
+
+    @get:Rule
+    val activity = ActivityScenarioRule(HomeActivity::class.java)
+
+    @Before
+    fun setup() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+    }
+
+    @After
+    fun teardown() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+    }
+
+    @Test
+    fun loadHeadlineNews_Success() {
+        onView(withId(R.id.rv_news)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_news)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(10))
+    }
+
+    @Test
+    fun loadDetailNews_Success() {
+        Intents.init()
+        onView(withId(R.id.rv_news)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        intended(hasComponent(NewsDetailActivity::class.java.name))
+        onView(withId(R.id.webView)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun loadBookmarkedNews_Success() {
+        onView(withId(R.id.rv_news)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.action_bookmark)).perform(click())
+        pressBack()
+        onView(withText("BOOKMARK")).perform(click())
+        onView(withId(R.id.rv_news)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_news)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.webView)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_bookmark)).perform(click())
+        pressBack()
+    }
+}
