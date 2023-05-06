bb0b8a952cf6b37674d14ff731a6f24543c01971
ricky-kiva <ricky.kiva@gmail.com>
2023-05-06 15:19:37 +0700

    add summary

--- app/src/main/java/com/dicoding/newsapp/ui/list/HomeActivity.kt
+++ app/src/main/java/com/dicoding/newsapp/ui/list/HomeActivity.kt
@@ -32,3 +32,12 @@ class HomeActivity : AppCompatActivity() {
         private val TAB_TITLES = intArrayOf(R.string.home, R.string.bookmark)
     }
 }
+
+// - TDD (Test Driven Development): a method that writes test code first before production code
+// - TDD Workflow:
+// - Start -> Write failing test -> Pass the test -> Refactor -> Back to start
+
+// - Write failing test: Confirm the test must fail with reason, because you haven't wrote any production code
+// - Write code that pass the test: Make production code to feed the test and pass
+// - Refactor -> Clean, simplify, optimize the logic & make the code modular
+// - Repeat: back write failing test when adding new functionality. Do commit as often as possible
